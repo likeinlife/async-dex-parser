04f8daafac51b50767f969f64bb4c6e025ad9dbe
likeinlife <likeinlife48@yandex.ru>
2023-03-07 18:42:32 +0300

    Fix bug with handling single extra chapters like `12.1`

--- app/title_parser/select_chapters.py
+++ app/title_parser/select_chapters.py
@@ -15,9 +15,11 @@ class SelectChapters:
     def __contains__(self, number: str) -> bool:
         """Check if this chapter in range"""
         include_flag = any(map(lambda x: self.__check_number(x, number), self.include))
-        exclude_flag = not any(map(lambda x: self.__check_number(x, number), self.exclude))
-        if include_flag and exclude_flag:
+        exclude_flag = any(map(lambda x: self.__check_number(x, number), self.exclude))
+        if include_flag and not exclude_flag:
+            logger.debug(f'Chapter {number} in range')
             return True
+        logger.debug(f'Chapter {number} not in range')
         return False
 
     def __make_lists(self):
@@ -28,16 +30,18 @@ class SelectChapters:
                 exclude.append(self.__make_range(item))
             else:
                 include.append(self.__make_range(item))
+        logger.debug(f'Including chapters {include=}')
+        logger.debug(f'Excluding {exclude=}')
         return include, exclude
 
     def __make_range(self, number: str):
         number = number.replace('~', '')
-        if number.isnumeric():
+        if number.replace('.', '').isnumeric():
             return float(number)
         elif '-' in number:
             return self.__get_start_and_end(number)
         else:
-            exit(f'Something went wrong with chapter range {number}')
+            exit(f'Incorrect range: {number}. Not a number nor range')
 
     def __get_start_and_end(self, chapter_range: str) -> Tuple[float, float]:
         """Transform '1-20' to tuple(1, 20)"""
@@ -46,13 +50,13 @@ class SelectChapters:
         if isinstance(start, float) and isinstance(end, float):
             return start, end
         else:
-            exit(f'Something went wrong with chapter range {chapter_range}')
+            exit(f'Not a digits {chapter_range}')
 
     @staticmethod
     def __check_number(chapter_range: float | Tuple[float, float], value_to_check: str) -> bool:
         """Check number if it is in :chapter_range:"""
         if isinstance(chapter_range, float):
-            if float(value_to_check) == chapter_range:
+            if value_to_check == str(chapter_range):
                 return True
         if isinstance(chapter_range, tuple):
             if chapter_range[0] <= float(value_to_check) <= chapter_range[1]:
