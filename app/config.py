89ce623a1c386e832f088e65b9fff628ff79d23c
likeinlife <likeinlife48@yandex.ru>
2023-03-07 18:21:23 +0300

    Add LOGGING_LEVEL change with env variable

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 __all__ = ['config']
 import logging
+import os
 
 
 class Config:
@@ -7,9 +8,17 @@ class Config:
     TRIES_NUMBER = 3
     SLEEP_BEFORE_RECONNECTION = 5  # seconds
     NAME_MAX_LENGTH = 40  # max length of title name
-    LOGGING_LEVEL = logging.INFO
     LOGS_MAX_SIZE = 256  # in bytes
     ENABLE_STREAM_HANDLER = False  # print logs to console
+
+    @property
+    def LOGGING_LEVEL(self):
+        if (var := os.getenv('LOGGING_LEVEL')):
+            return var
+        else:
+            return logging.INFO
+        
+
 
 
 config = Config()
